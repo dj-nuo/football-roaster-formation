7bcbb7e4a9a9cc272fd8bbc2711a3ddf04ec04e1
dj-nuo <djnuo95@gmail.com>
2022-08-02 21:02:03 +0200

    optimal time

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from roaster import *
 
-roaster = Roaster(players_json_file="players.json")
+roaster = Roaster(players_json_file="players.json", min_time_per_switch=1, max_time_per_switch=10)
 
-switch = print(roaster.generate_random_switch())
-roaster.generate_roaster()
+roaster.calculate_optimal_time_per_switch()
+
+# roaster.optimal_time_per_switch = 7
+# roaster.generate_roaster()
 roaster.calculate_switches_per_player(print_output=True)
-# roaster.generate_players_matrix()
+roaster.print_roaster()
+roaster.generate_players_matrix()
--- roaster.py
+++ roaster.py
@@ -2,13 +2,14 @@ import json
 import random
 import copy
 import pandas as pd
+import time
 
 # currently supports only 2 teams
 
 
 class Roaster:
-    def __init__(self, players_json_file, total_time_min=120, max_players_per_team=5):
-        self.players = json.load(open("players.json"))
+    def __init__(self, players_json_file, total_time_min=120, max_players_per_team=5, min_time_per_switch=5, max_time_per_switch=10):
+        self.players = json.load(open(players_json_file))
         self.player_names = [name["name"] for name in self.players]
         self.number_of_players = len(self.players)
         self.total_time_min = total_time_min
@@ -16,22 +17,34 @@ class Roaster:
         self.number_of_teams = 2
         self.max_players_per_team = max_players_per_team
         self.total_players_on_the_field = self.number_of_teams * self.max_players_per_team
-        self.roaster = []
+ 
         if self.number_of_players < self.total_players_on_the_field:
             raise Exception(
                 "Number of players cannot be less than total planned players on the field")
 
-        # TODO
-        # need to calculate optimal time per switch, so that everybody plays ~ the same amount of time
-        self.optimal_time_per__switch = 10
-        self.total_switches = int(
-            self.total_time_min / self.optimal_time_per__switch)
+        self.min_time_per_switch = min_time_per_switch
+        self.max_time_per_switch = max_time_per_switch
+
+    def calculate_optimal_time_per_switch(self):
+        for time_per_switch in range(self.min_time_per_switch, self.max_time_per_switch + 1):
+            self.optimal_time_per_switch = time_per_switch
+            self.total_switches = int(self.total_time_min / self.optimal_time_per_switch)
+            self.generate_roaster(print_output=False)
+
+            switches_per_player_deviation = self.calculate_switches_per_player_deviation()
+            if switches_per_player_deviation == 0:
+                print(self.optimal_time_per_switch, " minutes per switch. First found perfect result selected.")
+                return self.optimal_time_per_switch
+        print(time_per_switch, " minutes per switch. No perfect result found.")
+        return self.optimal_time_per_switch
+
 
     def generate_random_switch(self):
         random.shuffle(self.players)
         return self.players[0:self.total_players_on_the_field]
 
     def generate_roaster(self, print_output=True):
+        self.roaster = []
         for switch in range(self.total_switches):
             # keep brute forcing until balanced next switch is not found (where nobody is left out for more than 1 game)
             while True:
@@ -41,19 +54,20 @@ class Roaster:
                 else:
                     break
 
-        if print_output:
-            # calculating amount of games that each player plays "de facto"
-            # and generating roaster info for the user
-            for index, switch in enumerate(self.roaster):
-                # outputting every switch
-                # example:
-                #    1  | min 0-10 |  ['Emre', 'Other A', 'Luca', 'Luismi', 'Mohammad', 'Other B', 'Amir', 'Alex', 'Badreddine', 'Muath']
-                #    2  | min 10-20 |  ['Luismi', 'Alexander', 'Other A', 'Mohammad', 'Alex', 'Serhii', 'Luca', 'Ahmed Samir', 'Emre', 'Amir']
-                #    3  | min 20-30 |  ['Badreddine', 'Luismi', 'Other A', 'Other B', 'Muath', 'Luca', 'Amir', 'Ahmed Samir', 'Alexander', 'Mohammad']
-                print(index + 1, f" | min {index * self.optimal_time_per__switch}-{(index + 1) * self.optimal_time_per__switch} | ", [
-                    player["name"] for player in switch])
-
         return self.roaster
+
+    def print_roaster(self):
+        # calculating amount of games that each player plays "de facto"
+        # and generating roaster info for the user
+        for index, switch in enumerate(self.roaster):
+            # outputting every switch
+            # example:
+            #    1  | min 0-10 |  ['Emre', 'Other A', 'Luca', 'Luismi', 'Mohammad', 'Other B', 'Amir', 'Alex', 'Badreddine', 'Muath']
+            #    2  | min 10-20 |  ['Luismi', 'Alexander', 'Other A', 'Mohammad', 'Alex', 'Serhii', 'Luca', 'Ahmed Samir', 'Emre', 'Amir']
+            #    3  | min 20-30 |  ['Badreddine', 'Luismi', 'Other A', 'Other B', 'Muath', 'Luca', 'Amir', 'Ahmed Samir', 'Alexander', 'Mohammad']
+            print(index + 1, f" | min {index * self.optimal_time_per_switch}-{(index + 1) * self.optimal_time_per_switch} | ", [
+                player["name"] for player in switch])
+
 
     def calculate_switches_per_player(self, print_output=True):
         # example: {'Alex': 0, 'Emre': 0, 'Mohammad': 0, ...}
@@ -70,7 +84,7 @@ class Roaster:
         return switches_per_player
 
     def calculate_switches_per_player_deviation(self):
-        switches_per_player = self.calculate_switches_per_player()
+        switches_per_player = self.calculate_switches_per_player(print_output=False)
         all_numbers = []
         for player, switches in switches_per_player.items():
             all_numbers.append(switches)
